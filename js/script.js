bce18fd369194d1ce1fe667e272302e3580f0830
sadabx <sadabsiperkhan@gmail.com>
2025-12-20 01:47:37 +0600

    formspree

--- js/script.js
+++ js/script.js
@@ -43,12 +43,6 @@
     });
     
     // Form submission
-    // const form = document.querySelector('.form');
-    // form.addEventListener('submit', (e) => {
-    //   e.preventDefault();
-    //   alert('Thank you for your message! We will get back to you soon.');
-    //   form.reset();
-    // });
     
     // Animation on scroll
     const fadeElements = document.querySelectorAll('.fade-in');
